7903c1ceeb5ed2f4d47266ed27d9ffd0c14eedca
Christian Deacon <gamemann@gflclan.com>
2022-10-13 19:10:57 -0500

    Make progress on Linux server.

--- server-cli.gd
+++ server-cli.gd
@@ -1,6 +1,10 @@
 extends Control
 
 func _ready():
+	# Connect all the callbacks related to networking.
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+
 	# Set default host info.
 	var ip = "127.0.0.1"
 	var port = int(rand_range(1025, 65535))
@@ -30,7 +34,30 @@ func _ready():
 		return
 		
 	get_tree().set_network_peer(NetVars.server)
-	
+
+
+func _player_connected(_id):
+	# Someone connected, start the game!
+	var pong = load("res://pong.tscn").instance()
+	# Connect deferred so we can safely erase it from the callback.
+	pong.connect("game_finished", self, "_end_game", [], CONNECT_DEFERRED)
+
+	get_tree().get_root().add_child(pong)
+	hide()
+
+func _player_disconnected(_id):
+	print("Client disconnected")
+
+func _end_game(with_error = ""):
+	if has_node("/root/Pong"):
+		# Erase immediately, otherwise network might show
+		# errors (this is why we connected deferred above).
+		get_node("/root/Pong").free()
+		show()
+
+	get_tree().set_network_peer(null) # Remove peer.
+	print(with_error)
+
 func _process(delta):
 	# We can process commands later on!
 	pass
